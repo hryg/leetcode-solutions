39361c1b027276c2ec391f858412430f45df48b0
hrygddv <hrygheng@gmail.com>
2018-12-15 18:25:26 +0800

    :tada:004-寻找两个有序数组的中位数

---
+++ src/main/java/com/hryg/algorithm/hard/MedianofTwoSortedArrays.java
@@ -0,0 +1,41 @@
+package com.hryg.algorithm.hard;
+
+/**
+ * 寻找两个有序数组的中位数
+ *
+ * @author hryg
+ * Created on 2018-12-15
+ */
+public class MedianofTwoSortedArrays {
+
+    /**
+     * There are two sorted arrays nums1 and nums2 of size m and n respectively.
+     *
+     * Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+     *
+     * You may assume nums1 and nums2 cannot be both empty.
+     *
+     * Example 1:
+     *
+     * nums1 = [1, 3]
+     * nums2 = [2]
+     *
+     * The median is 2.0
+     * Example 2:
+     *
+     * nums1 = [1, 2]
+     * nums2 = [3, 4]
+     *
+     * The median is (2 + 3)/2 = 2.5
+     */
+    private static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        return 0D;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = new int[] {};
+        int[] nums2 = new int[] {};
+        System.out.println(findMedianSortedArrays(nums1, nums2));
+    }
+
+}
