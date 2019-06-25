46cc70ef22380ad209b03467216d0cdeec22c142
hrygddv <hrygheng@gmail.com>
2019-06-25 23:18:40 +0800

    最长回文子串

---
+++ src/main/java/com/hryg/algorithm/medium/LongestPalindromicSubstring.java
@@ -0,0 +1,33 @@
+package com.hryg.algorithm.medium;
+
+/**
+ * 最长回文子串
+ *
+ * @author hryg
+ * Created on 2019-06-25
+ */
+public class LongestPalindromicSubstring {
+
+    /**
+     * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+     *
+     * Example 1:
+     *
+     * Input: "babad"
+     * Output: "bab"
+     * Note: "aba" is also a valid answer.
+     * Example 2:
+     *
+     * Input: "cbbd"
+     * Output: "bb"
+     */
+    public static String longestPalindrome(String s) {
+        return "";
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
